5cdd055ed0f1ca9990365d749b3a76425636b00c
kenken520629 <j83859000@gmail.com>
2026-03-25 23:10:57 +0800

    Leetcode初體驗

---
+++ After-Class_Practices/Week1_01_LeetCode9/src/Leetcode9.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        bool isp=true;
+        int ssize = s.size();
+        vector<char> v(ssize , 0);
+        
+        for(int i=0; i<s.size(); ++i){
+            v[i]=s[ssize-1-i];
+        }
+        for(int i=0; i<s.size(); ++i){
+            if(v[i]!=s[i]){
+                isp = false;
+                break;
+            }
+        }
+        if(isp) return true;
+        else return false;
+    }
+};
